7fdde45c708e1a905eab2606d129bc1770c81a45
Subham-Das-98 <das198subham@gamil.com>
2024-11-19 03:38:38 +0530

    more clean error message in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,25 +6,28 @@ import User from "../models/user.model.js";
 const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     // get access token
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-  
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
     //verify token
-    if(!token) {
+    if (!token) {
       throw new ApiError(401, "Unauthorized request");
-  
     }
     // decode token
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-  
+
     // find user
-    const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
-  
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
     // verify user
-    if(!user) {
-      throw new ApiError(401, "Invalid access token");
+    if (!user) {
+      throw new ApiError(401, "User not found");
     }
-  
-    // set user
+
+    // set user in request object
     req.user = user;
     next();
   } catch (error) {
