46f8420c32e0ae87414c29b48451903455c0de7d
Subham-Das-98 <das198subham@gamil.com>
2025-01-10 21:49:14 +0530

    bug fix: minor origin issue

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 // MIDDLEWARE CONFIGURATION
-const allowedOrigins = ["http://192.168.235.214:5173", "http://localhost:5173", "http://example.com"];
+const allowedOrigins = [process.env.CORS_ORIGIN_1, process.env.CORS_ORIGIN_2, "http://example.com"];
 app.use(
   cors({
     origin: function (origin, callback) {
